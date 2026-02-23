59bd33bb913c8c9d3cbfb6cbc050c079d8edae66
Lianna-Mora01 <lianna.mora01@usa.edu.co>
2026-02-23 06:24:03 -0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+edad = int(input("Introduce tu edad: "))
+
+if edad < 16:
+    print("No puedes obtener licencia.")
+elif edad in (16, 17):
+    print("Puedes solicitar licencia de aprendiz.")
+else:  # edad >= 18
+    print("Puedes obtener licencia completa.")
